f8a7a9f579c0292f30bbe32daeca22e7e6ebf7cb
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-04-09 11:26:42 -0400

    Edit chat help message

--- src/chatCommand.js
+++ src/chatCommand.js
@@ -23,7 +23,7 @@ const execute = (command, user=false) => {
       response.message = 'Ok, I\'m deleting this ticket. :ticket: :x:\nIf you want to start another ticket, just send me another message, or use the /ticket command!';
       break;
     case('help'):
-      response.message = ':wave: Hi, I\'m Rob Bot, your friendly neighborhood web help robot! :robot_face:\nI\'m here to help you make a Freshdesk ticket for web support issues.\n\nHere are the commands you can give me:\n* `start` - Start a ticket! :white_check_mark:\n* `cancel` or `delete` - Delete the ticket currently in progress. :x:\n* `help` - See this helpful message again! :scroll:\n\nThat\'s it. Happy ticketing!';
+      response.message = ':wave: Hi, I\'m Rob Bot, your friendly neighborhood web help robot! :robot_face:\nI\'m here to help you make a Freshdesk ticket for web support issues.\n\nHere are the commands you can give me:\n* `ticket` - Start a ticket! :white_check_mark:\n* `cancel` - Delete the ticket currently in progress. :x:\n* `help` - See this helpful message again! :scroll:\n\nThat\'s it. Happy ticketing!';
       break;
   }
 
